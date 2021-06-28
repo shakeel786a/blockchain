67376057d6c2ee68fdf0991d7ae45ac3b1ff36ac
shakeel786a <ansari.shakeel0786@gmail.com>
2021-06-28 18:16:59 +0530

    add react-modal...

--- src/pages/web/Auth/Login.js
+++ src/pages/web/Auth/Login.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
+import Modal from 'react-modal'
 
 import { authAction } from '../../../actions'
 import { getAccount } from '../../../web3Integration/global.service'
@@ -27,6 +28,7 @@ function Login(props) {
                 if (isNewUser === false) {
                     dispatch({ type: authAction.SET_WEB_USER_INFO, payload: userInfo })
                 }
+                document.querySelector('body').classList.remove('modal-backdrop')
                 onClickClose()
             }
         }
@@ -51,8 +53,8 @@ function Login(props) {
     }
 
     return (
-        <section className="at-login-form">
-            <div className="modal fade" id="at-login" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div className='rmodal'>
+            <Modal isOpen={isVisible} contentLabel='Example Modal'>
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
@@ -77,8 +79,36 @@ function Login(props) {
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </Modal>
+        </div>
+        // <section className="at-login-form">
+        //     <div className="modal fade" id="at-login" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
+        //         <div className="modal-dialog" role="document">
+        //             <div className="modal-content">
+        //                 <div className="modal-header">
+        //                     <h3>Connect to Wallet</h3>
+        //                     <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={onClickClose}>
+        //                         <span aria-hidden="true">×</span>
+        //                     </button>
+        //                 </div>
+        //                 <div className="modal-body">
+        //                     <div className="form-group">
+        //                         <button type="button" className="w-100 btn btn-outline-success" onClick={onClickMetamask}>
+        //                             <img src_x="./webAssets/img/metamask.png" src="./webAssets/img/metamask.png" height="28" width="28" className="mr-1 v-align-middle" />
+        //                             MetaMask
+        //                         </button>
+        //                     </div>
+        //                     <div className="form-group mb-0">
+        //                         <button type="button" className="w-100 btn btn-outline-success">
+        //                             <img src_x="./webAssets/img/binance.png" src="./webAssets/img/binance.png" height="28" width="28" className="mr-1 v-align-middle" />
+        //                             BinanceChain
+        //                         </button>
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </section>
     )
 }
 
