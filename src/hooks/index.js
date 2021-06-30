273a04e3f5e721f9c490885173002ea0f7703577
shakeel786a <ansari.shakeel0786@gmail.com>
2021-06-30 15:17:44 +0530

    completed of metamask change id.

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,5 @@
 import useFetchAPI from './useFetchAPI'
 import useGetTokenIdAPI from './api/useGetTokenIdAPI'
+import useGetIsRegisterAPI from './api/useGetIsRegisterAPI'
 
-export { useFetchAPI, useGetTokenIdAPI }
+export { useFetchAPI, useGetTokenIdAPI, useGetIsRegisterAPI }
